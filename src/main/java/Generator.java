29a7f1db54e7f629017f5ee10727237a05e5d20b
dan <daniel_tzonkov@abv.bg>
2024-02-09 14:48:25 +0200

    09022024

--- src/main/java/Generator.java
+++ src/main/java/Generator.java
@@ -8,6 +8,7 @@ public class Generator {
         String [] carBrand = new String[]{"Volkswagen", "Mercedes", "Alfa Romeo", "Bentley", "Moscvitch", "Lada", "Nissan", "Peugeot", "Rolls-Royce", "Toyota", "Honda", "Ford", "Chevrolet", "BMW", "Audi", "Hyundai", "Kia", "Subaru", "Mazda", "Fiat", "Renault", "Skoda", "Volvo", "Jaguar", "Land Rover", "Porsche", "Tesla", "Mitsubishi", "Suzuki", "Citroen"};
         Arrays.sort(carBrand);
         String [] regionalCodes = new String[]{"E", "A", "B","BT", "BH", "BP", "EB", "K", "KH", "OB", "M", "PA", "PK", "EH", "PB", "PP", "P", "CC", "CH", "CO", "C", "CA", "CB", "CT", "T", "X", "H", "Y"};
+        String [] lastWord = new String[]{"А", "В", "Е", "К", "М", "Н", "О", "Р", "С", "Т", "У", "Х"};
 
         int numberOfCars = 10;
         System.out.println("Cars number: " + numberOfCars);
@@ -18,10 +19,14 @@ public class Generator {
             int randomNumber = random.nextInt(carBrand.length);
             String newCar = carBrand[randomNumber];
             String regionalCode = regionalCodes[random.nextInt(regionalCodes.length)];
-            int number = random.nextInt(9999);
+            int number = random.nextInt(1000, 9999);
 
             if (!cars.contains(newCar)) {
                 newCar = newCar + " " + regionalCode + number;
+                for (int i = 0; i < 2; i++) {
+                    int lastChar = random.nextInt(lastWord.length);
+                    newCar = newCar + lastWord[lastChar];
+                }
                 cars.add(newCar);
                 counter++;
             }
