1f5719b6a22a2c3ff0577e7b95fa374d5d3ac764
dan <daniel_tzonkov@abv.bg>
2024-02-09 11:39:43 +0200

    09022024

--- src/main/java/Generator.java
+++ src/main/java/Generator.java
@@ -7,35 +7,8 @@ public class Generator {
         List<String> cars = new ArrayList<>();
         String [] carBrand = new String[]{"Volkswagen", "Mercedes", "Alfa Romeo", "Bentley", "Moscvitch", "Lada", "Nissan", "Peugeot", "Rolls-Royce", "Toyota", "Honda", "Ford", "Chevrolet", "BMW", "Audi", "Hyundai", "Kia", "Subaru", "Mazda", "Fiat", "Renault", "Skoda", "Volvo", "Jaguar", "Land Rover", "Porsche", "Tesla", "Mitsubishi", "Suzuki", "Citroen"};
         Arrays.sort(carBrand);
+        String [] regionalCodes = new String[]{"E", "A", "B","BT", "BH", "BP", "EB", "K", "KH", "OB", "M", "PA", "PK", "EH", "PB", "PP", "P", "CC", "CH", "CO", "C", "CA", "CB", "CT", "T", "X", "H", "Y"};
         Map<String, String> regionalCodeOfCarNumber = new HashMap<>();
-        regionalCodeOfCarNumber.put("E", "Благоевград");
-        regionalCodeOfCarNumber.put("A", "Бургас");
-        regionalCodeOfCarNumber.put("B", "Варна");
-        regionalCodeOfCarNumber.put("BT", "Велико Търново");
-        regionalCodeOfCarNumber.put("BH", "Видин");
-        regionalCodeOfCarNumber.put("BP", "Враца");
-        regionalCodeOfCarNumber.put("EB", "Габрово");
-        regionalCodeOfCarNumber.put("K", "Кърджали");
-        regionalCodeOfCarNumber.put("KH", "Кюстендил");
-        regionalCodeOfCarNumber.put("OB", "Ловеч");
-        regionalCodeOfCarNumber.put("M", "Монтана");
-        regionalCodeOfCarNumber.put("PA", "Пазарджик");
-        regionalCodeOfCarNumber.put("PK", "Перник");
-        regionalCodeOfCarNumber.put("EH", "Плевен");
-        regionalCodeOfCarNumber.put("PB", "Пловдив");
-        regionalCodeOfCarNumber.put("PP", "Разград");
-        regionalCodeOfCarNumber.put("P", "Русе");
-        regionalCodeOfCarNumber.put("CC", "Силистра");
-        regionalCodeOfCarNumber.put("CH", "Сливен");
-        regionalCodeOfCarNumber.put("CO", "София-област");
-        regionalCodeOfCarNumber.put("C", "София-столица");
-        regionalCodeOfCarNumber.put("CA", "София-столица");
-        regionalCodeOfCarNumber.put("CB", "София-столица");
-        regionalCodeOfCarNumber.put("CT", "Стара Загора");
-        regionalCodeOfCarNumber.put("T", "Търговище");
-        regionalCodeOfCarNumber.put("X", "Хасково");
-        regionalCodeOfCarNumber.put("H", "Шумен");
-        regionalCodeOfCarNumber.put("Y", "Ямбол");
 
         int numberOfCars = 10;
         System.out.println("Cars number: " + numberOfCars);
@@ -44,6 +17,7 @@ public class Generator {
 
         while (counter < numberOfCars) {
             int randomNumber = random.nextInt(carBrand.length);
+            int regionalCode = random.nextInt(regionalCodes.length);
 
             if (!cars.contains(carBrand[randomNumber])) {
                 cars.add(carBrand[randomNumber]);
