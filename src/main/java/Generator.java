430c3e4bcdf72cc8d1d20eb8b8f14caeea3287e8
dan <daniel_tzonkov@abv.bg>
2024-02-09 10:41:09 +0200

    09022024

--- src/main/java/Generator.java
+++ src/main/java/Generator.java
@@ -1,18 +1,29 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class Generator {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        List<String> cars = new ArrayList<>();
         String [] carBrand = new String[]{"Volkswagen", "Mercedes", "Alfa Romeo", "Bentley", "Moscvitch", "Lada", "Nissan", "Peugeot", "Rolls-Royce", "Toyota", "Honda", "Ford", "Chevrolet", "BMW", "Audi", "Hyundai", "Kia", "Subaru", "Mazda", "Fiat", "Renault", "Skoda", "Volvo", "Jaguar", "Land Rover", "Porsche", "Tesla", "Mitsubishi", "Suzuki", "Citroen"};
         Arrays.sort(carBrand);
-        List<String> cars = new ArrayList<>();
 
-        Random random = new Random();
-        for (int i = 0; i < 10; i++) {
-            int randomNumber = random.nextInt(10);
-            System.out.println(carBrand[randomNumber]);
+        int numberOfCars = 10;
+        System.out.println("Cars number: " + numberOfCars);
+        //int numberOfCars = Integer.parseInt(scanner.nextLine());
+        int counter = 0;
+
+        while (counter < numberOfCars) {
+            int randomNumber = random.nextInt(carBrand.length);
+
+            if (!cars.contains(carBrand[randomNumber])) {
+                cars.add(carBrand[randomNumber]);
+                counter++;
+            }
+        }
+
+        for (int i = 0; i < cars.size(); i++) {
+            System.out.printf("%d %s\n", i + 1, cars.get(i));
         }
     }
 }
