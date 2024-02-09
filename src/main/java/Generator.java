bc926540cb64c54e85c9f6f3fcf3c2a022e2b221
dan <daniel_tzonkov@abv.bg>
2024-02-09 10:17:09 +0200

    09022024

--- src/main/java/Generator.java
+++ src/main/java/Generator.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 public class Generator {
     public static void main(String[] args) {
-        System.out.println("Test");
+        String [] carBrand = new String[]{"Volkswagen", "Mercedes", "Alfa Romeo", "Bentley", "Moscvitch", "Lada", "Nissan", "Peugeot", "Rolls-Royce", "Toyota", "Honda", "Ford", "Chevrolet", "BMW", "Audi", "Hyundai", "Kia", "Subaru", "Mazda", "Fiat", "Renault", "Skoda", "Volvo", "Jaguar", "Land Rover", "Porsche", "Tesla", "Mitsubishi", "Suzuki", "Citroen"};
+        Arrays.sort(carBrand);
+        List<String> cars = new ArrayList<>();
+
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            int randomNumber = random.nextInt(10);
+            System.out.println(carBrand[randomNumber]);
+        }
     }
 }
